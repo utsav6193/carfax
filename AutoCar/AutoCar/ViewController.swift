c8671c83bc33601f5731f477e4bb6b8357ae5fe2
Utsav Parikh <utsav6193@gmail.com>
2019-06-29 16:59:12 -0700

    inital project setup with deployment target iOS 12.2

---
+++ AutoCar/AutoCar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoCar
+//
+//  Created by Utsav Parikh on 6/29/19.
+//  Copyright © 2019 Carfax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
